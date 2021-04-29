56ac848b37c5ceb0d7231446d134992ebbd8c88a
Kriti Sen Sharma <ksen@paradigm4.com>
2021-04-29 10:48:02 +0530

    added files

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return(
+    <div style = {{ color: `teal` }}>
+    <Header headerText="About Gatsby"/>
+    <Header headerText="Hallelujah"/>
+    <p>Muhaha</p>
+    </div>
+  );
+}
